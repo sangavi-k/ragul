4b636f7332d35a0e66778c927edf3fd088cf6a1f
sangavi-k <sangavivalarmathi@gmail.com>
2017-07-05 10:42:47 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+# ragul
+import java.io.*;
+import java.util.*;
+public class Main{
+   	public static void main(String[] args) {
+    char c;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter an Alphabet");
+    c = sc.next().charAt(0);
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+      System.out.println(c + " is Vowel");
+    } else {
+      System.out.println(c + " is Consonant");
+    }
+  }
+}
